1b95a662ef41eb4a4a8c996d3f4c3e9683025d5b
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-06-19 10:39:55 +0200

    function

--- NaiveBaise.py
+++ NaiveBaise.py
@@ -36,7 +36,7 @@ def computelist(dfresult, list, pied, cur, filename):
     else:
         res =  math.floor(np.mean(list))
 
-    return dfresult.append({'video' : filename , 'pied' : pied, 'event' : current, 'frame': res}, ignore_index=True)
+    return dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': res}, ignore_index=True)
 
 def logistic(x_train, y_train):
     logisticRegr = LogisticRegression()
@@ -61,80 +61,154 @@ def MLP(x_train, y_train):
 
 
 
+def dataCollect(neighbours, directoire, dirpath):
+    k = 0
+    dfEventInit = pd.DataFrame(columns = ['video', 'pied','event','frame'])
+    for d in directoire:
+        for filename in os.listdir(dirpath + d):
+            path = dirpath + d + filename
+            reader = btk.btkAcquisitionFileReader()
+            reader.SetFilename(path)
+            reader.Update()
+            acq = reader.GetOutput()
+
+            mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
+            for nEve in range(100):
+                try:
+                    event = acq.GetEvent(nEve) # extract the first event of the aquisition
+                    #print(event.GetLabel()) # return a string representing the Label
+                    if (event.GetContext() == "Left"):
+                        pied = "left"
+                        argument = ['LTOE','LHEE','LPSI', 'LKNE']
+                        mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
+                        max_PSI = np.max(acq.GetPoint('LPSI').GetValues()[:,2])
+                        min_PSI = np.min(acq.GetPoint('LPSI').GetValues()[:,2])
+                        mean_TOE = np.mean(acq.GetPoint('LTOE').GetValues()[:,2])
+
+                    else:
+                        pied = "right"
+                        argument = ['RTOE','RHEE','RPSI', 'RKNE']
+                        mean_PSI = np.mean(acq.GetPoint('RPSI').GetValues()[:,2])
+                        max_PSI = np.max(acq.GetPoint('RPSI').GetValues()[:,2])
+                        min_PSI = np.min(acq.GetPoint('RPSI').GetValues()[:,2])
+                        mean_TOE = np.mean(acq.GetPoint('RTOE').GetValues()[:,2])
+
+                    frame = event.GetFrame()
+                    for i in neighbours: #[-15,0,15]: #
+                        if k == 0:
+                            x_train = np.array([acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
+                              acq.GetPoint(argument[1]).GetValues()[frame+i,2],acq.GetPoint(argument[2]).GetValues()[frame+i,2]-mean_PSI,
+                              acq.GetPoint(argument[3]).GetValues()[frame+i,2]-acq.GetPoint(argument[0]).GetValues()[frame+i,2]])
+                             # np.array([acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
+                             #  acq.GetPoint(argument[1]).GetValues()[frame+i,2],acq.GetPoint(argument[2]).GetValues()[frame+i,2]-mean_PSI])
+                             # #,acq.GetPoint('LANK').GetValues()[frame+i,2]
+                            if (i == 0):
+                                y_train = np.array([event.GetLabel()])
+                                dfEventInit = dfEventInit.append({'video' : filename , 'pied' : pied, 'event' : event.GetLabel(), 'frame': event.GetFrame()}, ignore_index=True)
+                            else:
+                                y_train = np.array(["Not_Event"])
+                            k = 1
+                        else:
+                            x_train = np.vstack([x_train, np.array([acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
+                            acq.GetPoint(argument[1]).GetValues()[frame+i,2],acq.GetPoint(argument[2]).GetValues()[frame+i,2]-mean_PSI,
+                            acq.GetPoint(argument[3]).GetValues()[frame+i,0]-acq.GetPoint(argument[0]).GetValues()[frame+i,0]])])
+                            if (i == 0):
+                                dfEventInit = dfEventInit.append({'video' : filename , 'pied' : pied, 'event' : event.GetLabel(), 'frame': event.GetFrame()}, ignore_index=True)
+                                y_train = np.vstack([y_train, np.array([event.GetLabel()])])
+                            else:
+                                y_train = np.vstack([y_train, np.array(["Not_Event"])])
+
+                except Exception as e:
+                    break
+    return x_train, y_train, dfEventInit
 
 
+def testmodel(model, directoire, dirpath):
+    dfresult = pd.DataFrame(columns = ['video', 'pied','event','frame'])
+
+    for d in directoire:
+        for filename in os.listdir(dirpath + d):
+            path = dirpath + d + filename
+
+            reader = btk.btkAcquisitionFileReader()
+            #path = 'Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d'
+            reader.SetFilename(path)
+            reader.Update()
+            acq = reader.GetOutput()
+            mean_TOE = np.mean(acq.GetPoint('LTOE').GetValues()[:,2])
+            data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('LTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('LHEE').GetValues()[:,2]]))), axis = 1)
+            mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
+            max_PSI = np.max(acq.GetPoint('LPSI').GetValues()[:,2])
+            min_PSI = np.min(acq.GetPoint('LPSI').GetValues()[:,2])
+            data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('LPSI').GetValues()[:,2]-mean_PSI]))), axis = 1)
+            data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('LKNE').GetValues()[:,0]-acq.GetPoint('LTOE').GetValues()[:,0]]))),axis = 1)
+            P =model.predict(data_FrameRef)
+            df = pd.DataFrame(data=P, columns = ['result'])
+            dfEvent = df.loc[df['result'] != 'Not_Event']
+            nligne = dfEvent.shape[0]
+            dfEvent['index'] = dfEvent.index
+
+            current = dfEvent.iloc[0,0]
+            comp_list = [dfEvent.iloc[0,1]]
+            for i in range(nligne-1):
+                if (current !=dfEvent.iloc[i+1,0]):
+                    dfresult = computelist(dfresult, comp_list, 'left', current, filename)
+                    current = dfEvent.iloc[i+1,0]
+                    comp_list = [dfEvent.iloc[i+1,1]]
+
+                else:
+                    comp_list.append(dfEvent.iloc[i+1,1])
+                if (i == nligne-2):
+                    dfresult = computelist(dfresult, comp_list, 'left', current, filename)
+
+
+
+
+    for d in directoire:
+        for filename in os.listdir(dirpath + d):
+            path = dirpath + d + filename
+
+            reader = btk.btkAcquisitionFileReader()
+            #path = 'Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d'
+            reader.SetFilename(path)
+            reader.Update()
+            acq = reader.GetOutput()
+            mean_TOE = np.mean(acq.GetPoint('RTOE').GetValues()[:,2])
+            data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('RTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('RHEE').GetValues()[:,2]]))), axis = 1)
+            mean_PSI = np.mean(acq.GetPoint('RPSI').GetValues()[:,2])
+            max_PSI = np.max(acq.GetPoint('RPSI').GetValues()[:,2])
+            min_PSI = np.min(acq.GetPoint('RPSI').GetValues()[:,2])
+            data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('RPSI').GetValues()[:,2]-mean_PSI]))), axis = 1)
+            data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('RKNE').GetValues()[:,0]-acq.GetPoint('RTOE').GetValues()[:,0]]))),axis = 1)
+            P =model.predict(data_FrameRef)
+            df = pd.DataFrame(data=P, columns = ['result'])
+            dfEvent = df.loc[df['result'] != 'Not_Event']
+            nligne = dfEvent.shape[0]
+            dfEvent['index'] = dfEvent.index
+
+            current = dfEvent.iloc[0,0]
+            comp_list = [dfEvent.iloc[0,1]]
+            for i in range(nligne-1):
+                if (current !=dfEvent.iloc[i+1,0]):
+                    dfresult = computelist(dfresult, comp_list, 'right', current, filename)
+                    current = dfEvent.iloc[i+1,0]
+                    comp_list = [dfEvent.iloc[i+1,1]]
+
+                else:
+                    comp_list.append(dfEvent.iloc[i+1,1])
+                if (i == nligne-2):
+                    dfresult = computelist(dfresult, comp_list, 'right', current, filename)
+
+    return dfresult
 
 dirpath = './Sofamehack2019/Sub_DB_Checked/'
 dir = ['FD/']
 
+[x_train, y_train, dfEventInit] = dataCollect([-15,-10,-5,0,5,10,15], dir, dirpath)
 
-
-f = 0
-k = 0
-kright = 0
-dfEventInit = pd.DataFrame(columns = ['video', 'pied','event','frame'])
-
-for d in dir:
-    for filename in os.listdir(dirpath + d):
-        path = dirpath + d + filename
-        reader = btk.btkAcquisitionFileReader()
-        reader.SetFilename(path)
-        reader.Update()
-        acq = reader.GetOutput()
-
-        mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
-        for nEve in range(100):
-            try:
-                event = acq.GetEvent(nEve) # extract the first event of the aquisition
-                #print(event.GetLabel()) # return a string representing the Label
-                if (event.GetContext() == "Left"):
-                    pied = "left"
-                    argument = ['LTOE','LHEE','LPSI', 'LKNE']
-                    mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
-                    max_PSI = np.max(acq.GetPoint('LPSI').GetValues()[:,2])
-                    min_PSI = np.min(acq.GetPoint('LPSI').GetValues()[:,2])
-                    mean_TOE = np.mean(acq.GetPoint('LTOE').GetValues()[:,2])
-
-                else:
-                    pied = "right"
-                    argument = ['RTOE','RHEE','RPSI', 'RKNE']
-                    mean_PSI = np.mean(acq.GetPoint('RPSI').GetValues()[:,2])
-                    max_PSI = np.max(acq.GetPoint('RPSI').GetValues()[:,2])
-                    min_PSI = np.min(acq.GetPoint('RPSI').GetValues()[:,2])
-                    mean_TOE = np.mean(acq.GetPoint('RTOE').GetValues()[:,2])
-
-                frame = event.GetFrame()
-                for i in [-15,-10,-5,0,5,10,15]: #[-15,0,15]: #
-                    if k == 0:
-                        x_train = np.array([acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
-                          acq.GetPoint(argument[1]).GetValues()[frame+i,2],acq.GetPoint(argument[2]).GetValues()[frame+i,2]-mean_PSI,
-                          acq.GetPoint(argument[3]).GetValues()[frame+i,2]-acq.GetPoint(argument[0]).GetValues()[frame+i,2]])
-                         # np.array([acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
-                         #  acq.GetPoint(argument[1]).GetValues()[frame+i,2],acq.GetPoint(argument[2]).GetValues()[frame+i,2]-mean_PSI])
-                         # #,acq.GetPoint('LANK').GetValues()[frame+i,2]
-                        if (i == 0):
-                            y_train = np.array([event.GetLabel()])
-                            dfEventInit = dfEventInit.append({'video' : filename , 'pied' : pied, 'event' : event.GetLabel(), 'frame': event.GetFrame()}, ignore_index=True)
-                        else:
-                            y_train = np.array(["Not_Event"])
-                        k = 1
-                    else:
-                        x_train = np.vstack([x_train, np.array([acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
-                        acq.GetPoint(argument[1]).GetValues()[frame+i,2],acq.GetPoint(argument[2]).GetValues()[frame+i,2]-mean_PSI,
-                        acq.GetPoint(argument[3]).GetValues()[frame+i,0]-acq.GetPoint(argument[0]).GetValues()[frame+i,0]])])
-                        if (i == 0):
-                            dfEventInit = dfEventInit.append({'video' : filename , 'pied' : pied, 'event' : event.GetLabel(), 'frame': event.GetFrame()}, ignore_index=True)
-                            y_train = np.vstack([y_train, np.array([event.GetLabel()])])
-                        else:
-                            y_train = np.vstack([y_train, np.array(["Not_Event"])])
-
-            except Exception as e:
-                break
-
-#print(dfEventInit)
 print('Taille de nos donnees : ',x_train.shape)
 
-X_train, X_test,Y_train,Y_test = train_test_split(x_train,y_train)
+#X_train, X_test,Y_train,Y_test = train_test_split(x_train,y_train)
 
 model = NaivesBayes(x_train,y_train)
 print(model.feature_importances_)
@@ -143,103 +217,12 @@ print(model.feature_importances_)
 #model = KNN(x_train,y_train)
 #model = MLP(x_train,y_train)
 
-# clf = tree.DecisionTreeClassifier()
-# clf = clf.fit(X_train,Y_train)
-# tree.plot_tree(clf)
-
+dfresult = testmodel(model, dir, dirpath)
 
 # dtree_predictions = model.predict(X_test)
 # cm = confusion_matrix(Y_test, dtree_predictions)
 # print(cm)
 # print(accuracy_score(Y_test,dtree_predictions))
-
-
-
-#Visualisation de notre image
-
-
-
-
-f = 0
-k = 0
-dfresult = pd.DataFrame(columns = ['video', 'pied','event','frame'])
-
-for d in dir:
-    for filename in os.listdir(dirpath + d):
-        path = dirpath + d + filename
-
-        reader = btk.btkAcquisitionFileReader()
-        #path = 'Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d'
-        reader.SetFilename(path)
-        reader.Update()
-        acq = reader.GetOutput()
-        mean_TOE = np.mean(acq.GetPoint('LTOE').GetValues()[:,2])
-        data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('LTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('LHEE').GetValues()[:,2]]))), axis = 1)
-        mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
-        max_PSI = np.max(acq.GetPoint('LPSI').GetValues()[:,2])
-        min_PSI = np.min(acq.GetPoint('LPSI').GetValues()[:,2])
-        data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('LPSI').GetValues()[:,2]-mean_PSI]))), axis = 1)
-        data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('LKNE').GetValues()[:,0]-acq.GetPoint('LTOE').GetValues()[:,0]]))),axis = 1)
-        P =model.predict(data_FrameRef)
-        df = pd.DataFrame(data=P, columns = ['result'])
-        dfEvent = df.loc[df['result'] != 'Not_Event']
-        nligne = dfEvent.shape[0]
-        dfEvent['index'] = dfEvent.index
-
-        current = dfEvent.iloc[0,0]
-        comp_list = [dfEvent.iloc[0,1]]
-        for i in range(nligne-1):
-            if (current !=dfEvent.iloc[i+1,0]):
-                dfresult = computelist(dfresult, comp_list, 'left', current, filename)
-                current = dfEvent.iloc[i+1,0]
-                comp_list = [dfEvent.iloc[i+1,1]]
-
-            else:
-                comp_list.append(dfEvent.iloc[i+1,1])
-            if (i == nligne-2):
-                dfresult = computelist(dfresult, comp_list, 'left', current, filename)
-
-
-f = 0
-k = 0
-kright = 0
-
-for d in dir:
-    for filename in os.listdir(dirpath + d):
-        path = dirpath + d + filename
-
-        reader = btk.btkAcquisitionFileReader()
-        #path = 'Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d'
-        reader.SetFilename(path)
-        reader.Update()
-        acq = reader.GetOutput()
-        mean_TOE = np.mean(acq.GetPoint('RTOE').GetValues()[:,2])
-        data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('RTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('RHEE').GetValues()[:,2]]))), axis = 1)
-        mean_PSI = np.mean(acq.GetPoint('RPSI').GetValues()[:,2])
-        max_PSI = np.max(acq.GetPoint('RPSI').GetValues()[:,2])
-        min_PSI = np.min(acq.GetPoint('RPSI').GetValues()[:,2])
-        data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('RPSI').GetValues()[:,2]-mean_PSI]))), axis = 1)
-        data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('RKNE').GetValues()[:,0]-acq.GetPoint('RTOE').GetValues()[:,0]]))),axis = 1)
-        P =model.predict(data_FrameRef)
-        df = pd.DataFrame(data=P, columns = ['result'])
-        dfEvent = df.loc[df['result'] != 'Not_Event']
-        nligne = dfEvent.shape[0]
-        dfEvent['index'] = dfEvent.index
-
-        current = dfEvent.iloc[0,0]
-        comp_list = [dfEvent.iloc[0,1]]
-        for i in range(nligne-1):
-            if (current !=dfEvent.iloc[i+1,0]):
-                dfresult = computelist(dfresult, comp_list, 'right', current, filename)
-                current = dfEvent.iloc[i+1,0]
-                comp_list = [dfEvent.iloc[i+1,1]]
-
-            else:
-                comp_list.append(dfEvent.iloc[i+1,1])
-            if (i == nligne-2):
-                dfresult = computelist(dfresult, comp_list, 'right', current, filename)
-
-
 
 nligneInit = dfEventInit.shape[0]
 diffTest = []
