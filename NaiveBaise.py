ebb2d4ea75ed5eb6c0af9a9bb1b09e3dc57c5cef
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-06-21 14:51:46 +0200

    triple sparse, result OK

--- NaiveBaise.py
+++ NaiveBaise.py
@@ -17,20 +17,19 @@ from sklearn.cluster import KMeans
 
 #calul the precise event and add one if it was forget
 def computelist(dfresult, list, pied, cur, filename):
-    if (filename == 'Cavus_Foot_01993_20130923_10.c3d'):
-        if (list[len(list)-1] - list[0] >= 30):
-            # On fait le centroid
-            kmeans = KMeans(n_clusters=2, random_state=0).fit(np.array(list).reshape(-1,1))
-            centroid = kmeans.cluster_centers_
-            dfresult = dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': math.floor(centroid[1][0])}, ignore_index=True)
-            dfresult = dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': math.floor(centroid[0][0])}, ignore_index=True)
-            if cur == 'Foot_Off_GS':
-                cur = 'Foot_Strike_GS'
-            else:
-                cur = 'Foot_Off_GS'
-            dfresult = dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': math.floor(np.mean(centroid))}, ignore_index=True)
+    if (list[-1] - list[0] >= 30):
+        # On fait le centroid
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(np.array(list).reshape(-1,1))
+        centroid = kmeans.cluster_centers_
+        dfresult = dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': math.floor(centroid[1][0])}, ignore_index=True)
+        dfresult = dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': math.floor(centroid[0][0])}, ignore_index=True)
+        if cur == 'Foot_Off_GS':
+            cur = 'Foot_Strike_GS'
+        else:
+            cur = 'Foot_Off_GS'
+        dfresult = dfresult.append({'video' : filename , 'pied' : pied, 'event' : cur, 'frame': math.floor(np.mean(centroid))}, ignore_index=True)
 
-            return dfresult
+        return dfresult
 
     if len(list) > 2:
         res = math.floor(np.mean(list[0:len(list)-1]))
@@ -78,11 +77,13 @@ def Data(neighbours, directoire, dirpath):
                         pied = "left"
                         argument = ['LTOE','LHEE','LPSI', 'LKNE']
                         mean_TOE = np.mean(acq.GetPoint('LTOE').GetValues()[:,2])
+                        mean_dif = np.mean(acq.GetPoint('LTOE').GetValues()[:,0]-acq.GetPoint('LHEE').GetValues()[:,0])
 
                     else:
                         pied = "right"
                         argument = ['RTOE','RHEE','RPSI', 'RKNE']
                         mean_TOE = np.mean(acq.GetPoint('RTOE').GetValues()[:,2])
+                        mean_dif = np.mean(acq.GetPoint('RTOE').GetValues()[:,0]-acq.GetPoint('RHEE').GetValues()[:,0])
 
                     frame = event.GetFrame()
                     for i in neighbours: #[-15,0,15]: #
@@ -91,7 +92,7 @@ def Data(neighbours, directoire, dirpath):
                             eventname = event.GetLabel()
                         data  = data.append({'video' : filename , 'pied' : pied, 'event' : eventname, 'frame': frame+i,
                         'TOEz-mean_TOEz': acq.GetPoint(argument[0]).GetValues()[frame+i,2]-mean_TOE,
-                        'TOEx-HEEx': acq.GetPoint(argument[0]).GetValues()[frame+i,0]-acq.GetPoint(argument[1]).GetValues()[frame+i,0],
+                        'TOEx-HEEx': acq.GetPoint(argument[0]).GetValues()[frame+i,0]-acq.GetPoint(argument[1]).GetValues()[frame+i,0]-mean_dif,
                          'KNEx-TOEx': acq.GetPoint(argument[3]).GetValues()[frame+i,0]-acq.GetPoint(argument[0]).GetValues()[frame+i,0]}, ignore_index=True)
 
                 except Exception as e:
@@ -121,7 +122,7 @@ def SplitData(pos, idset, idlist, filename):
     xTest = []
     for i in range(len(idset)):
         if (pos == 0):
-            if (i <= math.floor(len(idset)* (2.0/3.0))):
+            if (i <= math.ceil(len(idset)* (2.0/3.0))):
                 idTrain.append(idset[i])
             else:
                 idTest.append(idset[i])
@@ -131,7 +132,7 @@ def SplitData(pos, idset, idlist, filename):
             else:
                 idTest.append(idset[i])
         if (pos == 1):
-            if (i < math.floor(len(idset) * (1.0/3.0)) or i >= math.floor(len(idset) * (2.0/3.0))):
+            if (i <= math.floor(len(idset) * (1.0/3.0)) or i >= math.floor(len(idset) * (2.0/3.0))):
                 idTrain.append(idset[i])
             else:
                 idTest.append(idset[i])
@@ -150,23 +151,37 @@ def test(dataFrame, listTrain, listTest, dir , dirpath):
         dataTrain = dataTrain.append(dataFrame.loc[dataFrame['video'] == el])
     for el in listTest:
         dataTest = dataTest.append(dataFrame.loc[dataFrame['video'] == el])
+    print("training : ", listTrain)
+    print("test : ", listTest)
+    #model =  KNeighborsClassifier(n_neighbors = 3, metric = 'euclidean').fit(dataTrain[['TOEz-mean_TOEz','TOEx-HEEx', 'KNEx-TOEx']],dataTrain.event)
     model = DecisionTreeClassifier().fit(dataTrain[['TOEz-mean_TOEz','TOEx-HEEx', 'KNEx-TOEx']],dataTrain.event)
     dataframeresult = testmodel(model, dir, dirpath, listTest)
-    MeanExpo = []
-    MeanSum = []
     dfEvent = dataTest.loc[dataTest['event'] != "Not_Event"]
     nligneInit = dfEvent.shape[0]
-    diffTest = []
+    diffTestOff = []
+    diffTestStrick = []
     for el in range(nligneInit):
         value = np.min(np.abs(dataframeresult.loc[(dataframeresult['video'] == dfEvent.iloc[el,0]) & (dataframeresult['pied'] == dfEvent.iloc[el,1])
             & (dataframeresult['event'] == dfEvent.iloc[el,2]), 'frame'] - dfEvent.iloc[el,3]))
-        diffTest = np.append(diffTest, value)
+        if (dfEvent.iloc[el,2] == "Foot_Off_GS"):
+            diffTestOff = np.append(diffTestOff, value)
+        elif (dfEvent.iloc[el,2] == "Foot_Strike_GS"):
+            diffTestStrick = np.append(diffTestStrick, value)
+        if (math.isnan(value)):
+            print(el)
+            print(dfEvent.iloc[el,:])
 
-    print(model.feature_importances_)
-    MeanSum = np.append(MeanSum,  np.sum(diffTest))
-    MeanExpo = np.append(MeanExpo, np.sum(np.exp(diffTest), axis = 0))
-    erreur = 0
-    return MeanSum, MeanExpo
+
+    #print(model.feature_importances_)
+    MeanSumOff = np.sum(diffTestOff)
+    MeanExpoOff = np.sum(np.exp(diffTestOff), axis = 0)
+    MeanSumStrick = np.sum(diffTestStrick)
+    MeanExpoStrick = np.sum(np.exp(diffTestStrick), axis = 0)
+    print(MeanSumOff)
+    print(MeanExpoOff)
+    print(MeanSumStrick)
+    print(MeanExpoStrick)
+    return MeanSumOff, MeanExpoOff, MeanSumStrick, MeanExpoStrick
 
 
 
@@ -183,10 +198,9 @@ def testmodel(model, directoire, dirpath, listTest):
             reader.Update()
             acq = reader.GetOutput()
             mean_TOE = np.mean(acq.GetPoint('LTOE').GetValues()[:,2])
-            data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('LTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('LTOE').GetValues()[:,0]-acq.GetPoint('LHEE').GetValues()[:,0]]))), axis = 1)
-            mean_PSI = np.mean(acq.GetPoint('LPSI').GetValues()[:,2])
-            max_PSI = np.max(acq.GetPoint('LPSI').GetValues()[:,2])
-            min_PSI = np.min(acq.GetPoint('LPSI').GetValues()[:,2])
+            mean_dif = np.mean(acq.GetPoint('LTOE').GetValues()[:,0]-acq.GetPoint('LHEE').GetValues()[:,0])
+
+            data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('LTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('LTOE').GetValues()[:,0]-acq.GetPoint('LHEE').GetValues()[:,0]-mean_dif]))), axis = 1)
             #data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('LPSI').GetValues()[:,2]-mean_PSI]))), axis = 1)
             data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('LKNE').GetValues()[:,0]-acq.GetPoint('LTOE').GetValues()[:,0]]))),axis = 1)
             P =model.predict(data_FrameRef)
@@ -221,10 +235,10 @@ def testmodel(model, directoire, dirpath, listTest):
             reader.Update()
             acq = reader.GetOutput()
             mean_TOE = np.mean(acq.GetPoint('RTOE').GetValues()[:,2])
-            data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('RTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('RTOE').GetValues()[:,0]-acq.GetPoint('RHEE').GetValues()[:,0]]))), axis = 1)
-            mean_PSI = np.mean(acq.GetPoint('RPSI').GetValues()[:,2])
-            max_PSI = np.max(acq.GetPoint('RPSI').GetValues()[:,2])
-            min_PSI = np.min(acq.GetPoint('RPSI').GetValues()[:,2])
+            mean_dif = np.mean(acq.GetPoint('RTOE').GetValues()[:,0]-acq.GetPoint('RHEE').GetValues()[:,0])
+
+            data_FrameRef = np.concatenate((np.transpose(np.array([acq.GetPoint('RTOE').GetValues()[:,2]-mean_TOE])), np.transpose(np.array([acq.GetPoint('RTOE').GetValues()[:,0]-acq.GetPoint('RHEE').GetValues()[:,0]-mean_dif]))), axis = 1)
+
             #data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('RPSI').GetValues()[:,2]-mean_PSI]))), axis = 1)
             data_FrameRef = np.concatenate((data_FrameRef, np.transpose(np.array([acq.GetPoint('RKNE').GetValues()[:,0]-acq.GetPoint('RTOE').GetValues()[:,0]]))),axis = 1)
             P =model.predict(data_FrameRef)
@@ -245,19 +259,22 @@ def testmodel(model, directoire, dirpath, listTest):
                     comp_list.append(dfEvent.iloc[i+1,1])
                 if (i == nligne-2):
                     dfresult = computelist(dfresult, comp_list, 'right', current, filename)
-
     return dfresult
 
 
 dirpath = './Sofamehack2019/Sub_DB_Checked/'
-dir = ['ITW/']
+dir = ['CP/']
 
 framelist = [-9,0,9]
 dataFrame = Data(framelist, dir, dirpath)
 [filename, idPersonne, setPersonne] = ParseFileName(dataFrame)
-[listTrain, listTest] = SplitData(0, setPersonne, idPersonne, filename)
-[MeanSum, MeanExpo] = test(dataFrame, listTrain, listTest, dir , dirpath)
-
+MeanSumOff = np.zeros(3)
+MeanExpoOff = np.zeros(3)
+MeanSumStrick = np.zeros(3)
+MeanExpoStrick = np.zeros(3)
+for i in range(3):
+    [listTrain, listTest] = SplitData(i, setPersonne, idPersonne, filename)
+    [MeanSumOff[i], MeanExpoOff[i], MeanSumStrick[i], MeanExpoStrick[i]] = test(dataFrame, listTrain, listTest, dir , dirpath)
 # for i in range(10):
 #     [x_train, y_train, dfInit] = dataCollect(framelist, dir, dirpath)
 #     model = NaivesBayes(x_train,y_train)
@@ -305,6 +322,8 @@ dataFrame = Data(framelist, dir, dirpath)
     #     print(dfresult.loc[(dfresult['video'] == dfEventInit.iloc[el,0]) & (dfresult['pied'] == dfEventInit.iloc[el,1]) & (dfresult['event'] == dfEventInit.iloc[el,2])])
     #     print("\n\n")
     #diffTest = np.append(diff, np.min(np.abs(dfEventInit.loc[''])))
-
-print(" Erreur sum total : ", np.mean(MeanSum))
-print(" Erreur exponentiel : ", np.mean(MeanExpo))
+print(" list Mean Expo Off ", MeanExpoOff)
+print(" Erreur sum total Off: ", np.mean(MeanSumOff))
+print(" Erreur exponentiel Off: ", np.mean(MeanExpoOff))
+print(" Erreur sum total Strick: ", np.mean(MeanSumStrick))
+print(" Erreur exponentiel Strick: ", np.mean(MeanExpoStrick))
